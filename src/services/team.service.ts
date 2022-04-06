c90d59aeaf2d82f5a260ae4c39775c135abf6340
Max <maxprogrammer39@gmail.com>
2022-04-06 11:17:03 +0300

    fixed my issue with method post to get

--- src/services/team.service.ts
+++ src/services/team.service.ts
@@ -10,20 +10,20 @@ class TeamServices {
         const result = await TeamSchema.find({members: id});
         return result;
     }
-    async createOneTeam(team: ITeam) {
-        const result = await TeamSchema.create(team);
+    async findOneTeamByName(name: string) {
+        const result = await TeamSchema.findOne({name: name});
         return result;
     }
     async findOneTeamById(id: string) {
         const result = await TeamSchema.findOne({_id: id});
         return result;
     }
-    async findOneTeamByName(name: string) {
-        const result = await TeamSchema.findOne({name: name});
-        return result;
-    }
     async changeOneTeam(id: string, team: ITeam) {
         const result = await TeamSchema.updateOne({ _id: id }, team);
+        return result;
+    }
+    async createOneTeam(team: ITeam) {
+        const result = await TeamSchema.create(team);
         return result;
     }
     async deleteOneTeam(id: string) {
